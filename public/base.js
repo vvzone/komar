5e09ab658f6ce8cc9f2d4520a9c34fa62c20219e
vvzone <vvzone@yandex.ru>
2014-07-11 13:02:10 +0400

    add twitter bootstrap

---
+++ public/base.js
@@ -0,0 +1,76 @@
+/** @jsx React.DOM */
+/* som base elements */
+
+var ButtonEdit = React.createClass({
+    handleClick: function (e) {
+        var action = 'edit';
+        this.props.clicked(action);
+    },
+    render: function () {
+        //class="btn btn-sm btn-warning">
+        return ( <button className="ButtonEdit btn btn-xs btn-warning" type="button" onClick={this.handleClick}><span className="glyphicon glyphicon-pencil"></span>  Редактировать</button> );
+    }
+});
+
+var ButtonDelete = React.createClass({
+    handleClick: function (e) {
+        var action = 'delete';
+        this.props.clicked(action);
+    },
+    render: function () {
+        //class="btn btn-xs btn-danger"> Удалить</a>
+        return ( <button className="ButtonDelete btn btn-xs btn-danger" type="button" onClick={this.handleClick}><span className="glyphicon glyphicon-remove"></span>  Удалить</button> );
+    }
+});
+
+
+var ItemLink = React. createClass({
+    /*
+     * props: name, clicked()
+     *
+     * */
+
+    handleClick: function(e){
+        e.preventDefault();
+        this.props.clicked();
+    },
+    render: function(){
+        return(<a href="" onClick={this.handleClick}>{this.props.name}</a>)
+    }
+});
+
+
+var ListItems = React.createClass({
+    /*
+     * <ListItem item=[] key='' action={this.whenListItemsAction} />
+     * props:
+     * item = []; required: 'name', 'id'
+     * key - unique;
+     * */
+
+    whenClicked: function(){
+        this.props.itemdisplay(this.props.item.id);
+    },
+    whenClickedCP: function(action){
+        var itaction = [];
+        itaction['action'] = action;
+        itaction['id'] = this.props.item.id;
+        //console.log('whenClicked'+itaction['action']+' action[id]='+ itaction['id']);
+        this.props.itemaction(itaction);
+    },
+    render: function(){
+        var delete_key= 'delete/'+this.props.item.id;
+        var edit_key= 'edit/'+this.props.item.id;
+        return(
+            <div className="item">
+                <div className="item_name"><ItemLink name={this.props.item.name} clicked={this.whenClicked} /></div>
+                <div className="item_cp">
+                    <ButtonEdit clicked={this.whenClickedCP} id={this.props.item.id} key={edit_key}/>
+                    <ButtonDelete clicked={this.whenClickedCP} id={this.props.item.id} key={delete_key}/>
+                </div>
+            </div>
+            )
+    }
+});
+
+
--- public/test.js
+++ public/test.js
@@ -70,7 +70,7 @@ var CatLink = React. createClass({
         var src = './react/get/screen/'+link.screen;
         if(link.childNodes!=null){
             return(
-                    <li><CatTreeLinksList source={null} childs={link.childNodes}/></li>
+                    <li><i className="icon-user"></i><a href={src}>{link.name}</a><CatTreeLinksList source={null} childs={link.childNodes}/></li>
                 );
         }else{
             return(
@@ -112,78 +112,13 @@ var CatTreeLinksList = React. createClass({
             links_output.push(<CatLink screen ={link} key={link.id} />)
         });
         return(
-            <ul>{links_output}</ul>
+            <ul className="nav nav-list">{links_output}</ul>
         );
     }
 });
 
-var ButtonEdit = React.createClass({
-    handleClick: function (e) {
-        var action = 'edit';
-        this.props.clicked(action);
-    },
-    render: function () {
-        return ( <button className="ButtonEdit" type="button" onClick={this.handleClick}>Edit</button> );
-    }
- });
 
-var ButtonDelete = React.createClass({
-    handleClick: function (e) {
-        var action = 'delete';
-        this.props.clicked(action);
-    },
-    render: function () {
-        return ( <button className="ButtonDelete" type="button" onClick={this.handleClick}>Delete</button> );
-    }
-});
 
-var ItemLink = React. createClass({
-    /*
-    * props: name, clicked()
-    *
-    * */
-
-    handleClick: function(e){
-        e.preventDefault();
-      this.props.clicked();
-    },
-     render: function(){
-        return(<a href="" onClick={this.handleClick}>{this.props.name}</a>)
-    }
-});
-
-var ListItems = React.createClass({
-   /*
-   * <ListItem item=[] key='' action={this.whenListItemsAction} />
-   * props:
-   * item = []; required: 'name', 'id'
-   * key - unique;
-   * */
-
-     whenClicked: function(){
-         this.props.itemdisplay(this.props.item.id);
-     },
-     whenClickedCP: function(action){
-       var itaction = [];
-       itaction['action'] = action;
-       itaction['id'] = this.props.item.id;
-       //console.log('whenClicked'+itaction['action']+' action[id]='+ itaction['id']);
-       this.props.itemaction(itaction);
-   },
-   render: function(){
-       var delete_key= 'delete/'+this.props.item.id;
-       var edit_key= 'edit/'+this.props.item.id;
-       return(
-           <div className="item">
-               <div className="item_name"><ItemLink name={this.props.item.name} clicked={this.whenClicked} /></div>
-               <div className="item_cp">
-                   <ButtonEdit clicked={this.whenClickedCP} id={this.props.item.id} key={edit_key}/>
-                   <ButtonDelete clicked={this.whenClickedCP} id={this.props.item.id} key={delete_key}/>
-               </div>
-           </div>
-       )
-   }
-});
 
 
 var MainList = React. createClass({
