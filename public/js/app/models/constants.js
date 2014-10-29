612f4a039040cd55697c2811d1983cc6d806398a
vvzone <vvzone@yandex.ru>
2014-10-29 16:49:44 +0300

    little changes in constants

--- public/js/app/models/constants.js
+++ public/js/app/models/constants.js
@@ -2,18 +2,6 @@ define(
     'models/constants',
     [],function(){
         var constants = [];
-        /*
-         * Системные типы:
-         •	целое,  1
-         •	вещественное, 2
-         •	текст,  3
-         •	булевский, 4
-         •	Дата, 5
-         •	Время,  6
-         •	Дата/время, 7
-         •	Список,8
-         •	Составной.
-         * */
         var base_attr_type = [
             {id: 1, name: 'Целое'},
             {id: 2, name: 'Вещественное'},
@@ -25,8 +13,18 @@ define(
             {id: 8, name: 'Список'},
             {id: 9, name: 'Составной'}
         ];
-        constants['base_attr_type'] = base_attr_type;
 
+        var doc_state = [
+            {id: 1, name: 'Черновик'},
+            {id: 2, name: 'Совместная разработка'},
+            {id: 3, name: 'Сформирован'},
+            {id: 4, name: 'Исполнен'},
+            {id: 5, name: 'Отвергнут'},
+            {id: 5, name: 'Отозван'}
+        ];
+
+        constants['base_attr_type'] = base_attr_type;
+        constants['document_state'] = doc_state;
         return constants;
     }
 );
