1c6ed617261374b9a373f9102a9a10ad5d262070
vvzone <vvzone@yandex.ru>
2014-11-12 15:45:43 +0300

    levels some refactoring...

--- public/js/app/views/react/prototypes/levels.jsx
+++ public/js/app/views/react/prototypes/levels.jsx
@@ -58,12 +58,13 @@ define(
             componentWillMount: function() {},
             render: function() {
                 var output;
+                var self = this;
                 output = this.props.level_nodes_collection.map(function(node) {
                     return (
                         <LevelNode node={node}
-                            onDragStart={this.props.onDragStart}
-                            onDragStop={this.props.onDragStop}
-                            dragData={this.props.dragData}
+                            onDragStart={self.props.onDragStart}
+                            onDragStop={self.props.onDragStop}
+                            dragData={self.props.dragData}
                         />
                     );
                 });
@@ -80,11 +81,11 @@ define(
             },
             render: function() {
                 return(
-                    <div className="level">
-                        <div className="level_name">{this.props.model.get('name')}</div>
+                    <div className={this.classes()}>
+                        <div className="level_name">{this.props.level_model.get('name')}</div>
                         <div className="level_nodes">
                             <LevelNodes
-                                level_nodes_collection={this.props.model.get('nodes')}
+                                level_nodes_collection={this.props.level_model.get('nodes')}
                                 onDragStart={this.props.onDragStart}
                                 onDragStop={this.props.onDragStop}
                                 dragData={this.props.dragData}
@@ -92,6 +93,41 @@ define(
                         </div>
                     </div>
                 );
+            },
+            classes: function(){
+              console.info('...return classes...');
+              var classes =
+                  [
+                      'level drop-target',
+                      //"" + (this.props.target.accepts.join(' ')),
+                      this.active() ? 'active' : '',
+                      //this.active() && this.props.currentDragItem.type === 'green' ? 'active-green' : '',
+                      //this.active() && this.props.currentDragItem.type === 'blue' ? 'active-blue' : '',
+                      this.disabled() ? 'disabled' : '',
+                      this.state.hover ? 'hover' : ''
+                  ].join(' ');
+              console.info(classes);
+              return classes;
+            },
+            dragData: function(){
+                return {index: null};
+            },
+            active: function(){
+                //not current
+
+                var current_id = 5;//this.props.currentDragItem.get('id');
+                var level_nodes_collection = this.props.level_model.get('nodes');
+                var level_nodes_array = level_nodes_collection.toJSON();
+                var ids_array = _.pluck(level_nodes_array, 'id');
+
+                console.info('ids_array');
+                console.info(ids_array);
+
+                _.indexOf(ids_array, current_id);
+
+            },
+            disabled: function(){
+                //current
             }
         });
 
@@ -104,21 +140,32 @@ define(
                 });
                 */
 
-                output = this.props.levels_collection.map(function(model) {
+                var self = this;
+                output = this.props.levels_collection.map(function(level_model) {
                     console.log('level-model');
-                    console.log(model);
-                    return <Level model={model} />;
+                    console.log(level_model);
+                    return (
+                        <Level
+                            level_model={level_model}
+                            onDragStart={this.onDragStart}
+                            onDragStop={this.onDragStop}
+                            onDrop={this.onDrop}
+                            currentDragItem={self.props.currentDragItem}
+                        />
+                    );
+                    //this.props.currentDragItem
                 });
-
 
                 return <div>{output}</div>;
             }
         });
 
         var ListLevelsTable = React.createClass({
+            //MainComponent
             getInitialState: function() {
                 return {
-                    levels_collection: []
+                    levels_collection: [],
+                    currentDragItem: null
                 };
             },
             componentWillMount: function() {
@@ -143,7 +190,36 @@ define(
                 console.log(this.state.levels_collection);
                 //return <div><List levels_collection={this.state.levels_collection} /><div>Добавить</div></div>;
 
-                return <div><ListLevels levels_collection={this.state.levels_collection} /><div>Добавить</div></div>;
+                return (
+                    <div>
+                        <ListLevels
+                            levels_collection={this.state.levels_collection}
+                            onDragStart={this.onDragStart}
+                            onDragStop={this.onDragStop}
+                            onDrop={this.onDrop}
+                            currentDragItem={this.state.currentDragItem}
+                        />
+                        <div>Добавить</div>
+                    </div>
+                );
+            },
+            onDragStart: function(details){
+                this.setState({
+                    currentDragItem: details
+                });
+            },
+            onDragStop: function(){
+                this.setState({
+                    currentDragItem: null
+                });
+            },
+            onDrop: function(target){
+                this.setState({
+                    lastDrop: {
+                        source: this.state.currentDragItem,
+                        target: target
+                    }
+                });
             }
         });
 
