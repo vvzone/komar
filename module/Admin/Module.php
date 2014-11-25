c1688d223f01eb072b7f23f517bc46d1f8f0a286
vvzone <vvzone@yandex.ru>
2014-11-25 19:05:07 +0300

    JSON -> ZEND (problem) - not a problem at all... Zend 2 don't accept JSON request with no headers Content-Type: application/json

--- module/Admin/Module.php
+++ module/Admin/Module.php
@@ -21,6 +21,7 @@ use Object\Model\UnitTable;
 use Zend\Db\ResultSet\ResultSet;
 use Zend\Db\TableGateway\TableGateway;
 
+use Zend\View\Model\JsonModel;
 
 class Module
 {
@@ -50,6 +51,59 @@ class Module
         $eventManager        = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
+
+        $eventManager->attach(MvcEvent::EVENT_DISPATCH_ERROR, array($this, 'onDispatchError'), 0);
+        $eventManager->attach(MvcEvent::EVENT_RENDER_ERROR, array($this, 'onRenderError'), 0);
+    }
+
+    public function onDispatchError($e)
+    {
+        return $this->getJsonModelError($e);
+    }
+
+    public function onRenderError($e)
+    {
+        return $this->getJsonModelError($e);
+    }
+
+    public function getJsonModelError($e)
+    {
+        $error = $e->getError();
+        if (!$error) {
+            return;
+        }
+
+        $response = $e->getResponse();
+        $exception = $e->getParam('exception');
+        $exceptionJson = array();
+        if ($exception) {
+            $exceptionJson = array(
+                //'class' => get_class($exception),
+                'file' => $exception->getFile(),
+                //'line' => $exception->getLine(),
+                'message' => $exception->getMessage(),
+                //'stacktrace' => $exception->getTraceAsString(),
+                'code' => $exception->getCode()
+            );
+        }
+        if($exception->getCode()){
+            $response->setStatusCode($exception->getCode());
+        }
+
+        $errorJson = array(
+            'message'   => 'An error occurred during execution; please try again later.',
+            'error'     => $error,
+            'exception' => $exceptionJson,
+        );
+        if ($error == 'error-router-no-match') {
+            $errorJson['message'] = 'Resource not found.';
+        }
+
+        $model = new JsonModel(array('errors' => array($errorJson)));
+
+        $e->setResult($model);
+
+        return $model;
     }
 
     // Add this method:
@@ -71,6 +125,8 @@ class Module
             ),
         );
     }
+
+
 
 
 }
